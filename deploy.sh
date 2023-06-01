0c93d356561bb3eb2ea7d297215f78945ef3c52f
tamesisblade <53984711+tamesisblade@users.noreply.github.com>
2023-06-01 15:54:52 -0500

    eliminar fundacion

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-  #!/bin/bash
-cd /var/www/plataformaprolipa_server/ &&
-git add .
-git commit -m "server"
-git pull -q https://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@github.com/fernandocasame/plataformaprolipa_server
--- resources/views/contenido/estudiante.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('estudiante')
-@section('contenido')
-<template>
-    <header-component></header-component>
-</template>
-<template v-if="menu==5">
-    <estudientecurso-component></estudientecurso-component>
-</template>
-@endsection
