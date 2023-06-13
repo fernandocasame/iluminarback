a2db7ae97f4fb1c9be143bdc258eba88e31158d3
Fernando <fcasame@gmail.com>
2023-06-13 03:59:53 -0500

    scrip

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
   #!/bin/bash
 chown -R :nginx /var/www/html/iluminarback/iluminarback/storage/
-chown -R :nginx /var/www/html/iluminarback/iluminarback/bootstrap/cache/
+chown -R :nginx /var/www/html/iluminarback/iluminarback/bootstrap/
 chmod -R 0777 /var/www/html/iluminarback/iluminarback/storage/
-chmod -R 0775 /var/www/html/iluminarback/iluminarback/bootstrap/cache/
+chmod -R 0775 /var/www/html/iluminarback/iluminarback/bootstrap/
 semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/iluminarback/iluminarback/storage(/.*)?'
 semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/iluminarback/iluminarback/bootstrap/cache(/.*)?'
 restorecon -Rv '/var/www/html/iluminarback/iluminarback'
