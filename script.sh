4866e7abeca4452735a9d91a4107abd046d3a1f2
Fernando <fcasame@gmail.com>
2023-07-13 08:05:54 -0500

    permissions v2

--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@ chmod -R 0777 /var/www/html/iluminarback/iluminarback/storage/
 chmod -R 0777 /var/www/html/iluminarback/iluminarback/bootstrap/
 # semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/iluminarback/iluminarback/storage(/.*)?'
 # semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/iluminarback/iluminarback/bootstrap/cache(/.*)?'
-# restorecon -Rv '/var/www/html/iluminarback/iluminarback'
+restorecon -Rv '/var/www/html/iluminarback/iluminarback'
 sudo cp -rf /var/www/html/plataformaprolipa_server/vendor /var/www/html/iluminarback/iluminarback
