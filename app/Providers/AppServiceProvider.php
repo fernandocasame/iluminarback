b6891c1f7f5a718bbddcdf9a7b13f889b96019d7
Fernando Casame <fernandocasame7@gmail.com>
2024-03-28 10:06:01 -0500

    Update AppServiceProvider.php
    configuracion del vendor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('custom_password', function ($attribute, $value, $parameters, $validator) {
+            return Hash::check(md5($value), Auth::user()->password);
+        });
     }
 }
