7cadfa091afdc30d73fb86878a45dd1ee978f5ee
Fernando Casame <fernandocasame7@gmail.com>
2024-05-09 06:42:39 -0500

    Update auth.php
    sha1md5

--- config/auth.php
+++ config/auth.php
@@ -69,7 +69,7 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
-            'hash' => 'sha1md5',
+            'hash' => false,
         ],
 
         // 'users' => [
